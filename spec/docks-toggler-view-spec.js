b537fcce9868b85255bc132a08d86785cfc7d218
samthib <samuel.thibault@outlook.fr>
2021-11-08 18:45:28 +0100

    First commit

---
+++ spec/docks-toggler-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import DocksTogglerView from '../lib/docks-toggler-view';
+
+describe('DocksTogglerView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
